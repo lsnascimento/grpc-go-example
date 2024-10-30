9f643764a44b9543d0db46126bd0ef04ba826964
Leonardo Nascimento <leonardo.nascimento@directoai.com.br>
2024-10-30 11:56:40 -0300

    chore: implementation server and client

--- client/client.go
+++ client/client.go
@@ -1,16 +1,16 @@
-// package main
+package main
 
-// import (
-// 	"context"
-// 	"log"
+import (
+	"context"
+	"log"
 
-// 	pb "github.com/lsnascimento/grpc-go-example/greeter"
-// 	"google.golang.org/grpc"
-// )
+	pb "github.com/lsnascimento/grpc-go-example/greeter"
+	"google.golang.org/grpc"
+)
 
-// func main() {
-// 	conn, _ := grpc.Dial("localhost:50051", grpc.WithInsecure())
-// 	client := pb.NewGreeterClient(conn)
-// 	response, _ := client.SayHello(context.Background(), &pb.HelloRequest{Name: "World"})
-// 	log.Printf("Resposta: %s", response.Message)
-// }
+func main() {
+	conn, _ := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	client := pb.NewGreeterClient(conn)
+	response, _ := client.SayHello(context.Background(), &pb.HelloRequest{Name: "World"})
+	log.Printf("Resposta: %s", response)
+}
--- server/server.go
+++ server/server.go
@@ -1,24 +1,33 @@
-// package main
+package main
 
-// import (
-// 	"log"
-// 	"net"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
 
-// 	pb "github.com/lsnascimento/grpc-go-example/greeter"
-// 	"google.golang.org/grpc"
-// )
+	pb "github.com/lsnascimento/grpc-go-example/greeter"
+	"google.golang.org/grpc"
+)
 
-// type server struct {
-// 	pb.UnimplementedGreeterServer
-// }
+type server struct {
+	pb.UnimplementedGreeterServer
+}
 
-// func main() {
-// 	lis, err := net.Listen("tcp", ":50051")
-// 	if err != nil {
-// 		log.Fatalf("falha ao ouvir: %v", err)
-// 	}
-// 	s := grpc.NewServer()
-// 	pb.RegisterGreeterServer(s, &server{})
-// 	log.Printf("Servidor gRPC ouvindo na porta :50051")
-// 	s.Serve(lis)
-// }
+func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+	message := fmt.Sprintf("Hello, %s!", req.Name)
+	return &pb.HelloReply{Message: message}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("falha ao ouvir: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+
+	log.Printf("Servidor gRPC ouvindo na porta :50051")
+
+	s.Serve(lis)
+}
